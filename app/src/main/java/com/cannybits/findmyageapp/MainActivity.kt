b8734c8df3222198afd433e248770fe37f54d69f
nyandago <paulmutator@gmail.com>
2021-08-16 13:31:46 +0300

    App Code

--- app/src/main/java/com/cannybits/findmyageapp/MainActivity.kt
+++ app/src/main/java/com/cannybits/findmyageapp/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.cannybits.findmyageapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    btnGetAge.setOnClickListener(){
+        tvShowAge.text = "You are ${getUserAge().toString()} years old"
     }
+    }
+
+    private fun getUserAge():Int{
+        val userDOB = Integer.parseInt(etDate.text.toString())
+        val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+        return currentYear - userDOB
+    }
+
+
 }
