27992cf0af9ff4d3625afaa6daef2aa4df0c7cb2
nyandago <paulmutator@gmail.com>
2021-08-17 11:43:28 +0300

    Removed the Button, automatically calculate age per character input

--- app/src/main/java/com/cannybits/findmyageapp/MainActivity.kt
+++ app/src/main/java/com/cannybits/findmyageapp/MainActivity.kt
@@ -2,27 +2,44 @@ package com.cannybits.findmyageapp
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.text.Editable
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
+import android.text.TextWatcher
+import android.widget.Toast
 
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-    btnGetAge.setOnClickListener(){
-        getUserAge()
-    }
+        etDate.addTextChangedListener(textWatcher)
     }
 
-    @SuppressLint("SetTextI18n")
-    private fun getUserAge(){
-        val userDOB = Integer.parseInt(etDate.text.toString())
-        val currentYear = Calendar.getInstance().get(Calendar.YEAR)
-        tvShowAge.text ="You are ${currentYear - userDOB} years old"
+    private val textWatcher = object : TextWatcher {
+        override fun afterTextChanged(p0: Editable?) {
+
+        }
+
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+        }
+
+        @SuppressLint("SetTextI18n")
+        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            try {
+                val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+                val age = currentYear - Integer.parseInt(p0.toString())
+                tvShowAge.text = "You entered year ${p0}, you are ${age.toString()} years old"
+                if (p1 >= 4) {
+                    tvShowAge.text = "Invalid year"
+                }
+            } catch(ex: Exception){
+                tvShowAge.text = ex.message
+            }
+        }
     }
-
-
 }
